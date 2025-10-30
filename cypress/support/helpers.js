54e10149b70d5ad8717c2bd8aa8de211ad1130fa
Tainã Santos <santostainamd@gmail.com>
2025-10-30 17:44:47 -0400

    Setup inicial do cypress

---
+++ cypress/support/helpers.js
@@ -0,0 +1,28 @@
+import faker from "@faker-js/faker";
+
+export function getRandomNumber(){
+    return new Date().getTime()
+}
+
+export function getNewEmail(){
+    return `novo-qa-${getRandomNumber()}@mail.com`;
+}
+
+export function newRandomUser(){
+    return {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        address: faker.location.streetAddress(),
+        state: faker.location.state(),
+        city: faker.location.city(),
+        zCode: faker.location.zipCode('#####'),
+        mobileNumber: faker.phone.number(),
+    };
+}
+
+export function signUpForm(){
+    return {
+        name: faker.person.firstName(),
+        email: faker.internet.email(),
+    };
+}
